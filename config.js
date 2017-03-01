c88b5a91a39d2404d62246fc6a7082e48b292ebe
Matthew Newman <newmanm6@tcnj.edu>
2017-03-01 15:59:37 -0500

    included moment for date formatting easier

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ var requirejs = {
         ractive: "node_modules/ractive/ractive",
         'amd-loader': "node_modules/amd-loader/amd-loader",
         rv: "node_modules/rv/rv",
+        moment: "node_modules/moment/moment"
     }
 };
