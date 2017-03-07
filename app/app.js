fdbde0af4f5d9896d7b170636e14f113be3b3e95
Matthew Newman <newmanm6@tcnj.edu>
2017-03-07 17:57:18 -0500

    movement of axis horizontal allowed, if axis is out of range it disappears, date format changes,

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-define(['jquery', 'ractive', 'rv!templates/template', 'text!css/my-widget_embed.css','moment'], function ($, Ractive, template, css, moment) {
+define(['jquery', 'ractive', 'rv!templates/template', 'text!css/my-widget_embed.css','moment', 'jqueryUi'], function ($, Ractive, template, css, moment, autocomplete) {
 
   'use strict';
 
@@ -57,8 +57,51 @@ define(['jquery', 'ractive', 'rv!templates/template', 'text!css/my-widget_embed.
     //any variables not given in json are turned into empty strings
     json = this.cleanUpValues(json,errorChecking,jsonLevels);
     //console.log("Cleaned up json");
-    //console.log(json);
+    console.log(json);
 
+    var date_arr = [];
+    for(var j=0;j<json.events.length;j++) {
+        var formatted_date;
+
+        var day = json.events[j].start_date.day;
+        var month = json.events[j].start_date.month;
+        var year = json.events[j].start_date.year;
+
+        var date = year;
+        var perfect_date = true;
+
+        if(day === "00") {
+            date += "/01";
+            perfect_date = false;
+        }
+        else if(day < 10 && day != "00") {
+            date += "/0" + day;
+        }
+        else {
+            date += "/" + day;
+        }
+
+        if(month === "00") {
+            date += "/01";
+            perfect_date = false;
+        }
+        else if(month < 10 && month != "00") {
+            date += "/0" + month;
+        }
+        else {
+            date += "/" + month;
+        }
+
+        if(perfect_date) {
+            formatted_date = moment(new Date(date), moment.ISO_8601).format("dddd, MMMM Do YYYY");
+        }
+        else {
+            formatted_date = moment(new Date(date), moment.ISO_8601).format("MMMM, YYYY");
+        }
+        date_arr.push(formatted_date);
+    }
+    console.log(date_arr);
+    //var dates = this.getIndividualFormattedDates(json.);
 
     //gets all the colors necessary for styling everything
     //first number is which side to choose, second is which color
@@ -83,22 +126,103 @@ define(['jquery', 'ractive', 'rv!templates/template', 'text!css/my-widget_embed.
               height: height,
               json: json,
               sides: json.title.sides,
-              range: dateRange[0] + " - " + dateRange[1]
+              range: dateRange[0] + " - " + dateRange[1],
+              date_array: date_arr
             },
             oncomplete: function(){
-               //functions
+                //functions
+                var hide_axis = function(classToCheck, durationOfFade, startOrNah) {
+                    var original_line_left = $("._line").offset().left;
+                    var original_line_right = original_line_left + $("._line").width();
+
+                    $("." + classToCheck).each(function() {
+                        var offset = $(this).offset();
+                        var posLeft = offset.left - 15;
+                        var posRight = offset.left + 20;
+
+
+                        //left side
+                        if(posLeft < original_line_left) {
+                            //left side minor
+                            if(classToCheck === "_axisMinor") {
+                                $(this).css("opacity", 0);
+                            }
+                            //left side major
+                            else if (classToCheck === "_axisMajor"){
+                                $(this).css("background-color", "transparent");
+                                $(this).find("._YearMajor").hide();
+                            }
+
+                            //left side add the class
+                            $(this).addClass("_axis-hidden-left");
+                        }
+                        else if(posRight > original_line_right){
+                            //right side minor
+                            if(classToCheck === "_axisMinor") {
+                                $(this).css("opacity", 0);
+                            }
+                            //right side major
+                            else if (classToCheck === "_axisMajor"){
+                                $(this).css("background-color", "transparent");
+                                $(this).find("._YearMajor").hide();
+                            }
+
+                            //right side add the class
+                            $(this).addClass("_axis-hidden-right");
+                        }
+
+                        if($(this).hasClass("_axis-hidden-left")) {
+                            if(posLeft > original_line_left) {
+                                if(classToCheck === "_axisMinor") {
+                                    $(this).css("opacity", 1);
+                                }
+                                else if (classToCheck === "_axisMajor"){
+                                    $(this).css("background-color", "#000000");
+                                    $(this).find("._YearMajor").show();
+                                }
+
+                                //left side remove the class
+                                $(this).removeClass("_axis-hidden-left");
+                            }
+                        }
+                        else if($(this).hasClass("_axis-hidden-right")) {
+                            if(posRight < original_line_right) {
+                                if(classToCheck === "_axisMinor") {
+                                    $(this).css("opacity", 1);
+                                }
+                                else if (classToCheck === "_axisMajor"){
+                                    $(this).css("background-color", "#000000");
+                                    $(this).find("._YearMajor").show();
+                                }
+
+                                //right side remove the class
+                                $(this).removeClass("_axis-hidden-right");
+                            }
+                        }
+                    });
+                };
+
                 var whicheventfunc = function(which) {
                     var whichevent = curr_event.substr(curr_event.indexOf("-")+1,curr_event.length);
+                    var titleOrNah;
                     if(which==="left") {
                         if(whichevent === "title") {
                             curr_event = curr_event;
+                            titleOrNah = "title";
                         }
                         else if(whichevent === "0" || whichevent === 0) {
                             curr_event = curr_event.substr(0,curr_event.indexOf("-")+1) + "title";
+                            if(curr_event === "_event-title") {
+                                titleOrNah = "title";
+                            }
+                            else {
+                                titleOrNah = "not";
+                            }
                         }
                         else {
                             whichevent = parseInt(whichevent) - 1;
                             curr_event = curr_event.substr(0,curr_event.indexOf("-")+1) + whichevent;
+                            titleOrNah = "not";
                         }
                     }
                     else if(which==="right") {
@@ -112,93 +236,142 @@ define(['jquery', 'ractive', 'rv!templates/template', 'text!css/my-widget_embed.
                             whichevent = parseInt(whichevent) + 1;
                             curr_event = curr_event.substr(0,curr_event.indexOf("-")+1) + whichevent;
                         }
+                        titleOrNah = "not";
                     }
-                    setEvent(curr_event);
-                    setEverythingToColors(curr_color);
+                    setEvent(curr_event, curr_color, titleOrNah);
                 };
-                var setEverythingToColors = function(color) {
-                    $("._inner-info-container").css("background-color",color[0]);
-                    $("._inner-info-title-container").css("background-color",color[1]);
-                    $("._inner-info-container").css("-moz-box-shadow","5px 5px 5px " + color[2]).css("-webkit-box-shadow","5px 5px 5px " + color[2]).css("box-shadow","5px 5px 5px " + color[2]);
-                    $(".fa-caret-right, .fa-caret-left").css("color",color[3]);
-                    $("._active").css("color",color[3]);
-                    $("._event._active").css("background-color",color[3]);
-                };
+                var setEverythingToColors = function(color, notInit, event, duration) {
+                    var colorShadow = "5px 5px 5px " + color[2];
+                    if(notInit === "change") {
+                        $( "._inner-info-container." +event ).effect("fold",{},duration/2);
+                    }
+                    setTimeout(function(){
+                        if(notInit === "change") {
+                            $( "._inner-info-container."+event).fadeIn(duration);
+                        }
+                        $( "._inner-info-container" ).animate({
+                                backgroundColor:color[0],
+                        }, duration/2);
+                        $( "._inner-info-title-container" ).animate({
+                                backgroundColor:color[1],
+                        }, duration);
+                        $( ".fa-caret-right, .fa-caret-left" ).animate({
+                                color: color[3],
+                        }, duration);
+                    }, duration);
 
-                var setEvent = function(event) {
+
+
+                    //title circle
+                    $("._active").css("color", color[3]);
+                    //event circles
+                    $("._event._active").css("background-color", color[3]);
+
+                    //box shadows for inner square
+                    $("._inner-info-container").css("-moz-box-shadow",colorShadow).css("-webkit-box-shadow",colorShadow).css("box-shadow",colorShadow);
+                };
+                var setEvent = function(event, color, which) {
+                    var duration = 750;
+                    if(which === "not") {
+                        $(".title-icon,.title-icon-after").css("color","#000000");
+                    }
+
+                    $("._active").removeClass("_active _clicked");
+
+                    //if what needs to be set is not title or start
+                   if(which === "not") {
+                        $("div."+event+"._event").addClass("_clicked");
+                        setTimeout(function(){
+                            $("div."+event+"._event").addClass("_active _clicked");
+                            setEverythingToColors(curr_color, "change", event, duration);
+                        }, 100);
+                    }
+
+                    //if what needs to be set is the title screen
+                    else if(which === "title") {
+                        $(".title-icon").addClass("_clicked");
+                        setTimeout(function(){
+                            $(".title-icon").addClass("_active _clicked");
+                            $(".title-icon-after").addClass("_active _clicked");
+                            setEverythingToColors(curr_color, "change", event, duration);
+                        }, 100);
+                    }
+
+                    //if what needs to be set is in the beggining of the app
+                    else if(which === "start") {
+                        $(".title-icon").addClass("_active _clicked");
+                        $(".title-icon-after").addClass("_active _clicked");
+                        setEverythingToColors(curr_color, "start", event, duration);
+                    }
+
+
                     //makes sure all not correct events get hidden and are not active
                     $("div:not("+event+")._inner-info-container").hide();
-                    $("div:not("+event+")._inner-info-container").removeClass("active-event");
+                    $("div:not("+event+")._inner-info-container").removeClass("_active-event");
 
                     $("div:not(."+event+")._event").css("background-color","#000000");
 
                     //shows correct event and adds active class
                     $("div."+event+"._inner-info-container").show();
-                    $("div."+event+"._inner-info-container").addClass("active-event");
+                    $("div."+event+"._inner-info-container").addClass("_active-event");
                 };
-
                 var hideSides = function(side) {
                     side = side + 1;
                     $("div:not(.opinion-container-Side"+side+").opinion").hide();
                     $("div.opinion-container-Side" + side + ".opinion").show();
-                }
+                };
 
                 var curr_side = 0;
                 var curr_color = colors[curr_side];
                 var curr_event = "_event-title";
                 var didItGo = false;
+
                 //sets up app to start off properly
                 if(!didItGo) {
-                    setEvent(curr_event);
-                    setEverythingToColors(colors[curr_side]);
+                    $("._key-color1").addClass("_active-color");
+                    setEvent(curr_event, curr_color, "start");
                     hideSides(curr_side);
+                    hide_axis("_axisMinor", 200, "start");
+                    hide_axis("_axisMajor", 200, "start");
                     didItGo = true;
-
                 }
 
                 //if the legend is clicked
                 $('div[class ^= _side]').click(function() {
-                    curr_side = this.classList[0][5] - 1;
-                    curr_color = colors[curr_side];
-                    setEverythingToColors(colors[curr_side]);
-                    hideSides(curr_side);
+                    curr_side = this.classList[0][5];
+                    var event = $("._active-event").attr('class').split(" ")[1];
+                    var duration = 1000;
+
+                    if(!$("._key-color"+curr_side).hasClass("_active-color")) {
+                        $("._key-color"+curr_side).addClass("_active-color");
+                        curr_color = colors[curr_side-1];
+                        setEverythingToColors(curr_color, "change", event, duration);
+                        hideSides(curr_side-1);
+                        if(curr_side==="1") {
+                            curr_side = "2";
+                        }
+                        else {
+                            curr_side = "1";
+                        }
+                        $("._key-color"+curr_side).removeClass("_active-color");
+                    }
+                    else {
+                        $( "._inner-info-container." +event).effect("shake",{distance: 8},duration/2);
+                    }
                 });
 
                 //if the title icon is clicked
                 $('.title-icon').click(function() {
-                    $("._active").removeClass("_active _clicked");
                     curr_event = this.classList[1];
-
-                    setEvent(curr_event);
-
-
-                    var that = this;
-                    $(that).addClass("_clicked");
-                    setTimeout(function(){
-                        $(that).addClass("_active _clicked");
-                        $(".title-icon-after").addClass("_active _clicked");
-                        setEverythingToColors(curr_color);
-                    }, 100);
-
+                    setEvent(curr_event, curr_color, "title");
                 });
 
 
                 //if one of the circles on the timeline are clicked
                 $('div._event').click(function() {
-                    $(".title-icon,.title-icon-after").css("color","#000000");
-                    $("._active").removeClass("_active _clicked");
-
                     //changes current event to one that was clicked on
                     curr_event = this.classList[1];
-
-                    setEvent(curr_event);
-
-                    var that = this;
-                    $(that).addClass("_clicked");
-                    setTimeout(function(){
-                        $(that).addClass("_active");
-                        setEverythingToColors(curr_color);
-                    }, 100);
+                    setEvent(curr_event, curr_color, "not");
                 });
 
                 //right button clicked
@@ -210,22 +383,23 @@ define(['jquery', 'ractive', 'rv!templates/template', 'text!css/my-widget_embed.
                 $('.left-control').click(function() {
                     whicheventfunc("left");
                 });
+
+                $( "#_axis" ).draggable({ axis: "x" });
+
+
+                //Checks left property about twice per second
+                var watched = $("#_axis");
+                var lastDisplay = watched.css("left");
+                setInterval(function(){
+                  var curDisplay = watched.css("left");
+
+                  if (curDisplay!=lastDisplay){
+                    hide_axis("_axisMinor", 200, "update");
+                    hide_axis("_axisMajor", 200, "update");
+                    lastDisplay = curDisplay;
+                  }
+                },200);
             }
-          });
-          this.ractive.on({
-            mwClick: function(ev) {
-              ev.original.preventDefault()
-              this.set('count', this.get('count') + 1);
-              var that = this;
-              $.ajax({
-                url: "http://date.jsontest.com/",
-                dataType: "jsonp"
-              }).then(function(response) {
-                that.set("ts", response.time);
-              }, function(response) {
-                that.set("ts", "Something bad happened");
-              });
-            },
           });
       }, 500);
     },
@@ -359,7 +533,12 @@ define(['jquery', 'ractive', 'rv!templates/template', 'text!css/my-widget_embed.
                 }
                 for(var h=0;h<success[0].length;h++) {
                     if(nameOfSecondLevel != "sides") {
-                        funcData[nameOfFirstLevel][success[2][h]][nameOfSecondLevel][success[0][h]] = "";
+                        if(success[0][h] === "day" || success[0][h] === "month") {
+                            funcData[nameOfFirstLevel][success[2][h]][nameOfSecondLevel][success[0][h]] = "00";
+                        }
+                        else {
+                            funcData[nameOfFirstLevel][success[2][h]][nameOfSecondLevel][success[0][h]] = "";
+                        }
                     }
                     else {
                         var arrayForSides = Object.keys(funcData[nameOfFirstLevel][nameOfSecondLevel]);
@@ -453,17 +632,12 @@ define(['jquery', 'ractive', 'rv!templates/template', 'text!css/my-widget_embed.
             }
         }
 
-        var maxDate=new Date(Math.max.apply(null,dates));
-        var minDate=new Date(Math.min.apply(null,dates));
-
-        minandmax.push(new Date(Math.min.apply(null,dates)));
-        minandmax.push(new Date(Math.max.apply(null,dates)));
-        minandmax[0] = moment(minandmax[0], moment.ISO_8601).format("dddd, MMMM Do YYYY");
-        minandmax[1] = moment(minandmax[1], moment.ISO_8601).format("dddd, MMMM Do YYYY");
+        minandmax.push(moment(new Date(Math.min.apply(null,dates)), moment.ISO_8601).format("dddd, MMMM Do YYYY"));
+        minandmax.push(moment(new Date(Math.max.apply(null,dates)), moment.ISO_8601).format("dddd, MMMM Do YYYY"));
 
         if(err) {
-            console.log("Max Date: " + maxDate);
-            console.log("Min Date: " + minDate);
+            console.log("Max Date: " + minandmax[0]);
+            console.log("Min Date: " + minandmax[1]);
         }
         return minandmax;
     }
--- config.js
+++ config.js
@@ -2,6 +2,8 @@ var requirejs = {
     paths: {
         text : "node_modules/text/text",
         jquery: "node_modules/jquery/dist/jquery",
+        jqueryUi: "node_modules/jquery-ui-dist/jquery-ui",
+        jqueryColorAnimation: "node_modules/jquery-color-animation/jquery.animate-colors",
         ractive: "node_modules/ractive/ractive",
         'amd-loader': "node_modules/amd-loader/amd-loader",
         rv: "node_modules/rv/rv",
